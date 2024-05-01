f8a32c37f2853d38b21120988961c0731f5a76c2
HP3-16 <hp3.16.bsr@gmail.com>
2024-05-01 14:38:15 +0530

    precursor changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from keras.models import load_model
 
 st.title("Stock Prediction")
 
-user_input = st.text_input("Enter the Stock Symbol")
+user_input = st.text_input("Enter the Stock Symbol","Type Here")
 df = pd.read_csv(f'./equities/{user_input}.csv')
 
 st.subheader('Data till 2024')
@@ -22,13 +22,13 @@ y_predicted = model.predict(df_Test_X)
 
 y_predicted = y_predicted/scale
 df_Test_X = df_Test_X/scale
-
+split_date = '2023'
 st.subheader("Prediction vs Original")
 fig = plt.figure(figsize=(20,12))
 plt.plot(df['Date'][df['Date'] < '2023-06-01'], df['Adj Close'][df['Date'] < '2023-06-01'], label = 'Training')
 plt.plot(df['Date'][df['Date'] >= '2023-06-01'], df['Adj Close'][df['Date'] >= '2023-06-01'], label = 'Testing')
-plt.plot(df['Date'][df['Date'] >= '2023-06-13'], y_predicted.reshape(-1), label = 'Predictions')
-plt.xlim(pd.Timestamp("2020-01-01"),pd.Timestamp("2024-02-27"))
+plt.plot(df['Date'][df['Date'] >= '2023-06-14'], y_predicted.reshape(-1), label = 'Predictions')
+plt.xlim(pd.Timestamp("2020-01-01"),pd.Timestamp("2024-04-30"))
 plt.xlabel('Time')
 plt.ylabel('Closing Price')
 plt.legend(loc = 'best')
