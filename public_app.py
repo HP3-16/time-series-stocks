57b842cb3c3c83ce6a8e6eed6c4d581106271745
HP3-16 <hp3.16.bsr@gmail.com>
2024-05-01 17:23:05 +0530

    model uploaded

--- public_app.py
+++ public_app.py
@@ -19,8 +19,8 @@ df = pd.read_csv(StringIO(response.text))
 st.subheader('Data till 2024')
 st.write(df.describe())
 
-# model=load_model('models/basic_model.h5')
-model = TFSMLayer("mod",call_endpoint="serve")
+model=load_model('mod/saved_model.pb')
+# model = TFSMLayer("mod/saved_model.pb",call_endpoint="serve")
 ed_date = '2023-06-01'
 df["Date"] = pd.to_datetime(df["Date"])
 df_Train_X, df_Train_Y, df_Test_X, df_Test_Y,scale = DatasetGenerator.Dataset(df, ed_date)
