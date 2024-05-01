c67d71e9c7a800f64875669eeeeaaddb6e6558a8
HP3-16 <hp3.16.bsr@gmail.com>
2024-05-01 18:08:11 +0530

    model uploaded

--- public_app.py
+++ public_app.py
@@ -19,8 +19,8 @@ df = pd.read_csv(StringIO(response.text))
 st.subheader('Data till 2024')
 st.write(df.describe())
 
-# model=load_model('mod/saved_model.pb')
-model = tf.keras.layers.TFSMLayer("mod")
+model= keras.models.load_model("mod")
+# model = tf.keras.layers.TFSMLayer("mod")
 # ed_date = '2023-06-01'
 # df["Date"] = pd.to_datetime(df["Date"])
 # df_Train_X, df_Train_Y, df_Test_X, df_Test_Y,scale = DatasetGenerator.Dataset(df, ed_date)
